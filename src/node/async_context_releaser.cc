59724a11759b72c7bcbdc1eebbe7090765bbcc43
William Lahti <wilahti@gmail.com>
2026-06-27 01:36:38 -0700

    fix: asynccontextreleaser can crash during process exit

--- src/node/async_context_releaser.cc
+++ src/node/async_context_releaser.cc
@@ -3,6 +3,20 @@
 namespace node_webrtc {
 
 AsyncContextReleaser* AsyncContextReleaser::_default = nullptr;
+bool AsyncContextReleaser::_teardown = false;
+
+AsyncContextReleaser::~AsyncContextReleaser() {
+    if (_default == this) {
+        _default = nullptr;
+        _teardown = true;
+
+        // We intentionally leak _deferrer here!
+        // Node's environment teardown flushes pending libuv callbacks.
+        // By leaving this on the heap, the callback executes on a valid
+        // object, sees _parent is null, and safely does nothing.
+        _deferrer->Detach();
+    }
+}
 
 Napi::FunctionReference& AsyncContextReleaser::constructor() {
   static Napi::FunctionReference constructor;
@@ -13,14 +27,14 @@ void AsyncContextReleaser::Release(Napi::AsyncContext* context) {
   _contexts_mutex.lock();
   _contexts.push(context);
   _contexts_mutex.unlock();
-  Queue();
+  _deferrer->Queue();
 }
 
 void AsyncContextReleaser::Execute(Napi::Env env) {
   _contexts_mutex.lock();
   while (!_contexts.empty()) {
     Napi::HandleScope scope(env);
-    auto context = _contexts.front();
+    auto* context = _contexts.front();
     delete context;
     _contexts.pop();
   }
@@ -28,6 +42,8 @@ void AsyncContextReleaser::Execute(Napi::Env env) {
 }
 
 AsyncContextReleaser* AsyncContextReleaser::GetDefault() {
+  if (_teardown)
+    return nullptr;
   if (!_default) {
     Napi::HandleScope scope(constructor().Env());
     auto object = constructor().New({});
@@ -41,6 +57,26 @@ void AsyncContextReleaser::Init(Napi::Env env, Napi::Object) {
   auto func = DefineClass(env, "AsyncContextReleaser", {});
   constructor() = Napi::Persistent(func);
   constructor().SuppressDestruct();
+  
+  // If we wait for ~AsyncContextReleaser, it may be too late for the _teardown flag to be effective
+  // depending on finalization order. This hook ensures that we stop doing async context releases
+  // as soon as the process begins cleaning up.
+  env.AddCleanupHook([]() {
+    AsyncContextReleaser::_teardown = true;
+    if (_default)
+      _default->Execute(_default->Env());
+  });
+}
+
+void AsyncContextReleaser::SafeRelease(Napi::AsyncContext* context) {
+  auto* instance = GetDefault();
+  if (instance) {
+    instance->Release(context);
+  } else {
+    // We are in process teardown. The event loop is dead anyway,
+    // so just free the memory natively to prevent leaks.
+    delete context; 
+  }
 }
 
 }  // namespace node_webrtc
