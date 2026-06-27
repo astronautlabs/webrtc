8fe89a63435abb186097d4f7307cdde82a80e3f0
William Lahti <wilahti@gmail.com>
2026-06-27 01:37:15 -0700

    code style

--- src/dictionaries/node_webrtc/extended_rtc_configuration.h
+++ src/dictionaries/node_webrtc/extended_rtc_configuration.h
@@ -6,22 +6,25 @@
 #include "src/dictionaries/node_webrtc/unsigned_short_range.h"
 
 namespace node_webrtc {
+    struct ExtendedRTCConfiguration {
+        ExtendedRTCConfiguration() :
+            portRange(UnsignedShortRange()) 
+        {
+        }
 
-struct ExtendedRTCConfiguration {
-  ExtendedRTCConfiguration():
-    configuration(webrtc::PeerConnectionInterface::RTCConfiguration()),
-    portRange(UnsignedShortRange()) {}
+        ExtendedRTCConfiguration(
+            const webrtc::PeerConnectionInterface::RTCConfiguration& configuration,
+            const UnsignedShortRange portRange
+        ) :
+            configuration(configuration),
+            portRange(portRange) 
+        { 
+        }
 
-  ExtendedRTCConfiguration(
-      const webrtc::PeerConnectionInterface::RTCConfiguration& configuration,
-      const UnsignedShortRange portRange):
-    configuration(configuration),
-    portRange(portRange) {}
+        webrtc::PeerConnectionInterface::RTCConfiguration configuration;
+        UnsignedShortRange portRange;
+    };
 
-  webrtc::PeerConnectionInterface::RTCConfiguration configuration;
-  UnsignedShortRange portRange;
-};
+    DECLARE_TO_AND_FROM_NAPI(ExtendedRTCConfiguration)
 
-DECLARE_TO_AND_FROM_NAPI(ExtendedRTCConfiguration)
-
-}  // namespace node_webrtc
+} // namespace node_webrtc
