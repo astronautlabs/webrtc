f0497318a41207ccb7bdf9093612491746f9522f
William Lahti <wilahti@gmail.com>
2026-06-27 01:45:27 -0700

    async context releaser: use inner deferrer object

--- src/node/async_context_releaser.h
+++ src/node/async_context_releaser.h
@@ -8,29 +8,52 @@
 #include "src/node/deferrer.h"
 
 namespace node_webrtc {
+    class AsyncContextReleaser : public Napi::ObjectWrap<AsyncContextReleaser> {
+    public:
+        AsyncContextReleaser(const Napi::CallbackInfo& info) :
+            Napi::ObjectWrap<AsyncContextReleaser>(info), 
+            _deferrer(new ReleaserDeferrer(info.Env(), this))
+        {
+        }
 
-class AsyncContextReleaser
-  : public Napi::ObjectWrap<AsyncContextReleaser>
-  , private Deferrer {
- public:
-  AsyncContextReleaser(const Napi::CallbackInfo& info)
-    : Napi::ObjectWrap<AsyncContextReleaser>(info)
-    , Deferrer(info.Env()) {}
+        ~AsyncContextReleaser() override;
 
-  static AsyncContextReleaser* GetDefault();
-  static void Init(Napi::Env, Napi::Object);
+        static AsyncContextReleaser* GetDefault();
+        static void Init(Napi::Env, Napi::Object);
 
-  void Release(Napi::AsyncContext*);
+        static void SafeRelease(Napi::AsyncContext* context);
 
- protected:
-  void Execute(Napi::Env);
+        void Release(Napi::AsyncContext*);
 
- private:
-  static AsyncContextReleaser* _default;
-  static Napi::FunctionReference& constructor();
+    protected:
+        void Execute(Napi::Env);
 
-  std::queue<Napi::AsyncContext*> _contexts;
-  std::mutex _contexts_mutex{};
-};
+    private:
+        class ReleaserDeferrer : public Deferrer {
+        public:
+            ReleaserDeferrer(Napi::Env env, AsyncContextReleaser* parent): 
+                Deferrer(env), _parent(parent) 
+            {
+            }
+            
+            void Execute(Napi::Env env) override {
+                if (_parent) {
+                    _parent->Execute(env);
+                }
+            }
+            
+            void Detach() { _parent = nullptr; }
+        private:
+            AsyncContextReleaser* _parent;
+        };
 
-}  // namespace node_webrtc
+        static AsyncContextReleaser* _default;
+        static bool _teardown;
+        static Napi::FunctionReference& constructor();
+
+        ReleaserDeferrer* _deferrer;
+        std::queue<Napi::AsyncContext*> _contexts;
+        std::mutex _contexts_mutex;
+    };
+
+} // namespace node_webrtc
