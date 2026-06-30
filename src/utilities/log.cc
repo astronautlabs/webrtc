4fdf6b346304c43e39cac42c66f23d0645c0fe51
William Lahti <wilahti@gmail.com>
2026-06-29 23:40:54 -0700

    log: auto class name, better formatting, static variant

--- src/utilities/log.cc
+++ src/utilities/log.cc
@@ -6,16 +6,4 @@
 namespace node_webrtc {
     bool hasConfiguredLogging = false;
     bool loggingEnabled = false;
-    void Log(void* Owner, std::string message) {
-        if (!hasConfiguredLogging) {
-            hasConfiguredLogging = true;
-            const char* logVar = std::getenv("NODE_WEBRTC_LOGGING");
-            if (logVar && std::string { logVar } == "1") {
-                loggingEnabled = true;
-            }
-        }
-
-        if (loggingEnabled)
-            std::cerr << "@/webrtc native [" << std::hex << reinterpret_cast<uint64_t>(Owner) << std::dec << "] " << message << std::endl;
-    }
 } // namespace node_webrtc
--- src/utilities/log.h
+++ src/utilities/log.h
@@ -1,5 +1,47 @@
+#pragma once
+
+#include "src/utilities/nameof.h"
+#include <iostream>
 #include <string>
 
 namespace node_webrtc {
-    void Log(void* Owner, std::string message);
+    extern bool hasConfiguredLogging;
+    extern bool loggingEnabled;
+
+    template <typename T>
+    void Log(std::string message) {
+        if (!hasConfiguredLogging) {
+            hasConfiguredLogging = true;
+            const char* logVar = std::getenv("NODE_WEBRTC_LOGGING");
+            if (logVar && std::string { logVar } == "1") {
+                loggingEnabled = true;
+            }
+        }
+
+        if (loggingEnabled)
+            std::cerr 
+                << std::string { NAMEOF_TYPE(T) } << " -> " 
+                << message 
+                << std::endl;
+    }
+
+    template <typename T>
+    void Log(T* Owner, std::string message) {
+        if (!hasConfiguredLogging) {
+            hasConfiguredLogging = true;
+            const char* logVar = std::getenv("NODE_WEBRTC_LOGGING");
+            if (logVar && std::string { logVar } == "1") {
+                loggingEnabled = true;
+            }
+        }
+
+        if (loggingEnabled)
+            std::cerr 
+                << std::string { NAMEOF_TYPE(T) } << ":"
+                    << std::hex << reinterpret_cast<uint64_t>(Owner) << std::dec 
+                << " -> "
+                << message 
+                << std::endl;
+
+    }
 } // namespace node_webrtc
