7bf79e1988ae4c9d99a94132211f41704a1f13df
William Lahti <wilahti@gmail.com>
2026-07-11 14:39:49 -0700

    simplify build script: always generate compile_commands, move back to build/ folder for workspace, but keep extra builds under build/platform-arch

--- scripts/build-from-source.mjs
+++ scripts/build-from-source.mjs
@@ -21,11 +21,12 @@ function main(args) {
     const debugMode = process.env.DEBUG === '1';
     const platform = os.platform();
     const arch = process.env.TARGET_ARCH ?? os.arch();
-    const buildFolder = isDevWorkspace ? `build/dev` : `build/${platform}-${arch}`;
-    const cmakeJsArgs = ["-O", buildFolder, "-a", arch];
-
-    if (isDevWorkspace)
-        cmakeJsArgs.push("--CDCMAKE_EXPORT_COMPILE_COMMANDS=1");
+    const buildFolder = isDevWorkspace ? `build` : `build/${platform}-${arch}`;
+    const cmakeJsArgs = [
+        "-O", buildFolder, 
+        "-a", arch,
+        "--CDCMAKE_EXPORT_COMPILE_COMMANDS=1"
+    ];
 
     if (isDevWorkspace || debugMode)
         cmakeJsArgs.push("--debug");
