624131938f4c383109c3c3bf424161ea95bef714
William Lahti <wilahti@gmail.com>
2026-07-11 14:38:55 -0700

    electron test works

--- lib/nodejs/test/electron/index.ts
+++ lib/nodejs/test/electron/index.ts
@@ -1,6 +1,11 @@
-/* eslint no-process-exit:0 */
 import { RTCPeerConnection } from '../..';
+
+// We're just testing that the module links with Electron here.
+// NOTE: Currently we support Node.js 18 and later. Electron v42+
+// no longer supports Node.js 18, so we test with v41.
 
 const pc = new RTCPeerConnection();
 pc.close();
+
+console.log(`✅ Looks good!`);
 process.exit();
