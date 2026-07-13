5e06deff2666ab89806b023a13411db68e8eb0f3
William Lahti <wilahti@gmail.com>
2026-07-13 13:15:48 -0700

    dont report on uncaught exception

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,4 @@
-// Add this at the very top of your test or main file
 process.report.reportOnFatalError = true;
-process.report.reportOnUncaughtException = true;
-// Optional: trigger it on a specific signal
+process.report.reportOnUncaughtException = false;
 process.report.reportOnSignal = true; 
 process.report.signal = 'SIGSEGV';
