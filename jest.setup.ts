a91ed831ff5a377f21a1c9c7006c75ce08c7997c
William Lahti <wilahti@gmail.com>
2026-07-13 12:41:07 -0700

    ci: use builtin node.js reports + core dumps

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,6 @@
-// Ensure segfaults are traced in CI
-import '@astronautlabs/segfault-handler';
+// Add this at the very top of your test or main file
+process.report.reportOnFatalError = true;
+process.report.reportOnUncaughtException = true;
+// Optional: trigger it on a specific signal
+process.report.reportOnSignal = true; 
+process.report.signal = 'SIGSEGV';
