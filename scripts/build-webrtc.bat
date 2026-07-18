210b447ade9011542b256679e6f81c1260a932ad
William Lahti <wilahti@gmail.com>
2026-07-18 02:24:18 -0700

    ci: win: attempt to fix broken cached webrtc junction just after build (since PRE_BUILD step is actually PRE_LINK on ninja generator)
    TODO: remove the PRE_BUILD step from CMakeLists.txt, not doing it now to avoid cache getting invalidated

--- scripts/build-webrtc.bat
+++ scripts/build-webrtc.bat
@@ -3,6 +3,8 @@
 echo ==========================================================
 echo Building libwebrtc
 echo ==========================================================
+echo
+echo Working directory: %CD%
 
 set PATH=%DEPOT_TOOLS%;%PATH%
 set TARGETS=libjingle_peerconnection builtin_video_encoder_factory builtin_video_decoder_factory media_engine rtc_software_fallback_wrappers
@@ -10,6 +12,17 @@ set TARGETS=libjingle_peerconnection builtin_video_encoder_factory builtin_video
 
 @ECHO ON
 call ninja webrtc %TARGETS% -j 24 -d explain || goto :error
+
+:: CircleCI's `tar` breaks NTFS Directory Junctions when saving/restoring cache.
+:: We must safely delete the broken cache artifact and recreate the junction 
+:: before moving on to the addon build to ensure headers resolve correctly.
+node -e "const fs=require('node:fs'); fs.rmSync('../webrtc', {recursive:true, force:true});" >nul 2>&1
+pushd ..
+if not exist "webrtc\" (
+    echo Repairing WebRTC directory junction for Windows CI Cache...
+    mklink /J webrtc src
+)
+popd
 
 : ------------------------------------------------------------------------------------
 goto :EOF
